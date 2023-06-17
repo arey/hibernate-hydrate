dae0a2219e05b63a2137ce1147a513f76ed7abed
Markus Heiden <markus@markusheiden.de>
2023-06-17 13:54:19 +0200

    Start removing the dependency to Spring framework

--- src/test/java/com/javaetmoi/core/persistence/hibernate/DBUnitLoader.java
+++ src/test/java/com/javaetmoi/core/persistence/hibernate/DBUnitLoader.java
@@ -14,15 +14,13 @@
 package com.javaetmoi.core.persistence.hibernate;
 
 import java.io.IOException;
-import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.dbunit.DatabaseUnitException;
-import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseDataSourceConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.DataSetException;
@@ -31,18 +29,20 @@ import org.dbunit.dataset.NoSuchTableException;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.ext.h2.H2DataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.apache.commons.lang3.ArrayUtils.isEmpty;
+import static org.dbunit.database.DatabaseConfig.PROPERTY_DATATYPE_FACTORY;
+import static org.dbunit.operation.DatabaseOperation.DELETE_ALL;
+import static org.dbunit.operation.DatabaseOperation.INSERT;
 
 /**
  * Allows to easily insert and cleanup test data into a database.
  * 
- * @author arey
- * 
+ * @author Antoine Rey
+ * @author Markus Heiden
  */
 public class DBUnitLoader {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DBUnitLoader.class);
 
     private final DataSource dataSource;
 
@@ -54,108 +54,70 @@ public class DBUnitLoader {
      * Generate a default location based on the name of the given class. If the class is named
      * com.example.MyTest, DBUnitLoad loads your DBUnit dataset from
      * "com/example/MyTest-dataset.xml".
-     * 
+     *
      * @param testClass
+     *         Test class.
      */
     public void loadDatabase(Class<?> testClass) {
-        loadDatabase(buildDefaultDataSetLocation(testClass));
-    }
-
-    private String buildDefaultDataSetLocation(Class<?> testClass) {
-        return testClass.getName().replace(".", "/") + "-dataset.xml";
+        var dataSetLocation = testClass.getName().replace(".", "/") + "-dataset.xml";
+        loadDatabase(dataSetLocation);
     }
 
     public void loadDatabase(String... dataSetLocations) {
-        var dataSets = new ArrayList<IDataSet>();
-
-        if ((dataSetLocations == null) || (dataSetLocations.length == 0)) {
+        if (isEmpty(dataSetLocations)) {
             throw new IllegalArgumentException("Dataset location is mandatory");
         }
 
-        for (var dataSetLocation : dataSetLocations) {
-            var url = retrieveDataSetURL(dataSetLocation);
+        var dataSets = Arrays.stream(dataSetLocations)
+                .map(this::buildDataSet)
+                .collect(toUnmodifiableList());
+
+        var connection = connection();
+        executeAll(connection, DELETE_ALL, dataSets);
+        executeAll(connection, INSERT, dataSets);
+    }
+
+    private IDataSet buildDataSet(String dataSetLocation) {
+        var url = getClass().getClassLoader().getResource(dataSetLocation);
+        if (url == null) {
+            throw new IllegalArgumentException("No dataSet file located at " + dataSetLocation);
+        }
+
+        try {
             var flatXmlDataSetBuilder = new FlatXmlDataSetBuilder();
             flatXmlDataSetBuilder.setColumnSensing(true);
-            IDataSet dataSet;
-            try {
-                dataSet = flatXmlDataSetBuilder.build(url.openStream());
-            } catch (DataSetException | IOException e) {
-                throw new RuntimeException("Error while reading dataset " + dataSetLocation, e);
-            }
-            dataSets.add(dataSet);
+            return flatXmlDataSetBuilder.build(url.openStream());
+        } catch (DataSetException | IOException e) {
+            throw new RuntimeException("Error while reading dataset " + dataSetLocation, e);
         }
-        IDatabaseConnection dbConn;
+    }
+
+    private IDatabaseConnection connection() {
         try {
-            dbConn = new DatabaseDataSourceConnection(dataSource);
+            var connection = new DatabaseDataSourceConnection(dataSource);
+            var config = connection.getConfig();
+            config.setProperty(PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+            return connection;
         } catch (SQLException e) {
             throw new RuntimeException("Getting JDBC data source", e);
         }
-        var config = dbConn.getConfig();
-        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
-        try {
-            executeDeleteAll(dataSets, dbConn);
-            executeInsert(dataSets, dbConn);
+    }
+
+    public void executeAll(IDatabaseConnection connection, DatabaseOperation operation, List<IDataSet> dataSets) {
+        try (var c = dataSource.getConnection(); var statement = c.createStatement()) {
+            // language=H2
+            statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
+            for (var dataSet : dataSets) {
+                operation.execute(connection, dataSet);
+            }
+            // language=H2
+            statement.execute("SET REFERENTIAL_INTEGRITY TRUE");
+        } catch (NoSuchTableException e) {
+            throw new RuntimeException("Differences between dataset tables and hibernate configuration, check your dataset (typing error), did you override at least one of these beans : mappingResources, annotatedClasses", e);
         } catch (DatabaseUnitException e) {
             throw new RuntimeException("DBUnit error", e);
         } catch (SQLException e) {
             throw new RuntimeException("Inserting DBUnit dataset", e);
         }
-
-    }
-
-    private void executeDeleteAll(List<IDataSet> dataSets, IDatabaseConnection dbConn)
-            throws DatabaseUnitException, SQLException {
-        new AbstractDatabaseOperation() {
-
-            @Override
-            protected void execute(IDataSet dataSet, IDatabaseConnection dbConn)
-                    throws DatabaseUnitException, SQLException {
-                DatabaseOperation.DELETE_ALL.execute(dbConn, dataSet);
-            }
-
-        }.execute(dataSets, dbConn);
-    }
-
-    private void executeInsert(List<IDataSet> dataSets, IDatabaseConnection dbConn)
-            throws DatabaseUnitException, SQLException {
-        new AbstractDatabaseOperation() {
-
-            @Override
-            protected void execute(IDataSet dataSet, IDatabaseConnection dbConn)
-                    throws DatabaseUnitException, SQLException {
-                DatabaseOperation.INSERT.execute(dbConn, dataSet);
-            }
-
-        }.execute(dataSets, dbConn);
-    }
-
-    private URL retrieveDataSetURL(String dataSetLocation) {
-        var url = getClass().getClassLoader().getResource(dataSetLocation);
-        if (url == null) {
-            throw new IllegalArgumentException("No dataSet file located at " + dataSetLocation);
-        }
-        return url;
-    }
-
-    abstract class AbstractDatabaseOperation {
-
-        public void execute(List<IDataSet> dataSets, IDatabaseConnection dbConn)
-                throws DatabaseUnitException, SQLException {
-            try (var connection = dataSource.getConnection(); var statement = connection.createStatement()) {
-                // language=H2
-                statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
-                for (var dataSet : dataSets) {
-                    execute(dataSet, dbConn);
-                }
-                // language=H2
-                statement.execute("SET REFERENTIAL_INTEGRITY TRUE");
-            } catch (NoSuchTableException e) {
-                LOG.error("Differences between dataset tables and hibernate configuration, check your dataset (typing error), did you override at least one of these beans : mappingResources, annotatedClasses");
-                throw e;
-            }
-        }
-
-        protected abstract void execute(IDataSet dataSet, IDatabaseConnection dbConn)
-                throws DatabaseUnitException, SQLException;
     }
 }
